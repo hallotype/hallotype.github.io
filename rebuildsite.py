f66a65b4989dc14aedb18f58a17a309c4f29dd1c
hallotype <ohamburgefonstiv@gmail.com>
2020-06-02 23:03:49 +0200

    next

--- rebuildsite.py
+++ rebuildsite.py
@@ -55,12 +55,6 @@ fontcssFile.close()
 htmlFile = open("templateindex.html", "r")
 htmlFileTxt = htmlFile.read()
 htmlFile.close()
-
-headerFile = open("header.html", "r")
-headerFileTxt = headerFile.read()
-headerFile.close()
-
-
 htmlIndex = open("site/index.html", "w+")
-htmlIndex.write(htmlFileTxt.format(header=headerFileTxt, typebody=typebody))
+htmlIndex.write(htmlFileTxt.format(typebody=typebody))
 htmlIndex.close()
--- site/js/code.js
+++
@@ -1,2 +0,0 @@
-let word;
-word = "OHambrugje";
