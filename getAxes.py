7027192c023d2227e8e1da5c1d0dc7bdc0df5d12
hallotype <ohamburgefonstiv@gmail.com>
2020-06-09 23:17:37 +0200

    start implement var-font

---
+++ getAxes.py
@@ -0,0 +1,23 @@
+import glob
+import os
+from fontTools.ttLib import TTFont
+fonts = {}
+for ttf in glob.glob("fonts/**.ttf"):
+    ftf = TTFont(ttf)
+    fontname = os.path.basename(ttf)[:-4]
+    axes = []
+    # print(fontname)
+    if 'fvar' in ftf.keys():
+        axes = []
+        for axis in ftf['fvar'].axes:
+            a = {}
+            a['tag'] = axis.axisTag
+            a['min'] = axis.minValue
+            a['def'] = axis.defaultValue
+            a['max'] = axis.maxValue
+            axes.append(a)
+    if axes:
+        fonts[fontname] = axes
+d = open("axes", "w+")
+d.write(str(fonts))
+d.close()
--- processFonts.sh
+++ processFonts.sh
@@ -6,15 +6,18 @@ base64Dir=$targetFolder"/BASE64"
 rm -rf $targetFolder/
 mkdir -p $woff2Dir $base64Dir
 
-# find all otf ad ttf
+rm axes
+
+# find all otf ad ttf 
 for font in $(find . -type f -name "*.*tf") 
 do
   filename=$(basename -- "$font")
   extension="${filename##*.}"
   filename="${filename%.*}"
   echo $font
-  echo $filename
+  # echo $filename
 
-  fonttools ttLib.woff2 compress $font -o $woff2Dir/$filename.woff2 # gen woff2 out of otf
-  base64 $woff2Dir/$filename.woff2 > $base64Dir/$filename.b64
+  # fonttools ttLib.woff2 compress $font -o $woff2Dir/$filename.woff2 # gen woff2 out of otf
+  # base64 $woff2Dir/$filename.woff2 > $base64Dir/$filename.b64
 done
+python3 getAxes.py
