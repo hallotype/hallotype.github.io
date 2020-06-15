2003f8e9412fc8f74f17f55648acef86871e3b7b
hallotype <ohamburgefonstiv@gmail.com>
2020-06-15 13:56:20 +0200

    test1

--- js/varSupport.js
+++ js/varSupport.js
@@ -1,15 +1,19 @@
-
 const PanSliders = document.querySelector("#sliders-Pan");
 PanSliders.oninput = PanChanger;
 function PanChanger(e) {
   let fontElements = document.querySelectorAll("div.Pan p");
   fontElements.forEach(function (fontElement) {
+    console.log(fontElement);
     fontElement.style.cssText =
       "font-variation-settings:" +
-"'wght' " + document.querySelector("#Panwght").value
-+",'ANGL' " + document.querySelector("#PanANGL").value
-+",'STEP' " + document.querySelector("#PanSTEP").value
-  });}
+      "'wght' " +
+      document.querySelector("#Panwght").value +
+      ",'ANGL' " +
+      document.querySelector("#PanANGL").value +
+      ",'STEP' " +
+      document.querySelector("#PanSTEP").value;
+  });
+}
 const bexSliders = document.querySelector("#sliders-bex");
 bexSliders.oninput = bexChanger;
 function bexChanger(e) {
@@ -17,5 +21,7 @@ function bexChanger(e) {
   fontElements.forEach(function (fontElement) {
     fontElement.style.cssText =
       "font-variation-settings:" +
-"'wght' " + document.querySelector("#bexwght").value
-  });}
+      "'wght' " +
+      document.querySelector("#bexwght").value;
+  });
+}
