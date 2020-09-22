e99e21e0aeeb9551aa76071ea60ccc7db18b34a6
hallotype <thomjanssen128@gmail.com>
2020-09-22 15:03:16 +0200

    update Wave>Golf + cleanup

--- content/wave/texts.py
+++ content/wave/texts.py
@@ -1,16 +1,15 @@
-title = 'Ripple Type'
+title = 'golf'
 
 tags = '''type
 variable
 experiment'''
 
 description = '''This is a fine example where variable fonts and animation go hand in hand.<br>
-The phase of a wave repeats over and over to create a smooth animation. Other parameters in this design are the amplitute, wave-length, the speed of the phase, and the direction of the phase. <br><br>
+The phase of a wave repeats over and over to create a smooth animation. Other parameters in this design are the amplitute, wave-length, speed, and direction. <br><br>
 
 I wrote a tool to create this font out of a normal font. To declare the extreme for the amplitude, and all the wave-lengths, the wave-font is generated.<br> '''
 
 from collections import OrderedDict
 sd = OrderedDict()
 sitecontent = OrderedDict()
-sitecontent["img01.png"] = "some text"
-sitecontent["water.gif"] = "some text"
+sitecontent["cover.gif"] = "some text"
