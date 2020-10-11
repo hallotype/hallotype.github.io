330a0035562fdf30a5bf64b56105001b23662bc2
hallotype <thomjanssen128@gmail.com>
2020-10-11 19:14:45 +0200

    pan update axes explain

--- content/pan/texts.py
+++ content/pan/texts.py
@@ -6,13 +6,14 @@ variable
 experiment'''
 
 description = '''Pan is a variable type system. The idea is to slice the glyphs in various ways and draw contours with the slice-data. <br>
-Since the drawings are not compatible, featureSubstitutions are needed. Currently three axes [Angle, Steps, Shape] controle what glyph is shown. A special set of tools was needed to create the GSUB table that can handle this.<br><br>
+Since the drawings are not compatible, featureSubstitutions are needed. Currently five axes [Steps, Angle, Shape, Offset, Inner] controle what glyph is shown. A special set of tools was needed to create the GSUB table that can handle this.<br><br>
 
 '''
 
 sd = OrderedDict()
 sitecontent = OrderedDict()
 sitecontent["cover.png"] = "some text"
+sitecontent["PanAxes.html"] = "PA"
 sitecontent['coretext.html'] = 'ct'
 sitecontent["SWITCH.gif"] = "some text"
 sitecontent['clock.html'] = "hoi"
