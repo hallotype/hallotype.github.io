8b30499554cdeb9989b74977ace9c42b45a4bbad
hallotype <thomjanssen128@gmail.com>
2020-12-11 21:36:04 +0100

    accents fresh content

---
+++ content/accents/texts.py
@@ -0,0 +1,23 @@
+title = 'accents'
+
+tags = '''workflow
+ufo
+robofont
+scripting'''
+
+description = '''
+This is my approach on a flexible accents workflow.<br>
+It uses <a href='https://github.com/typemytype/GlyphConstruction'> GlyphConstruction</a>. 
+<br><br>
+'''
+
+from collections import OrderedDict
+sitecontent = OrderedDict()
+sitecontent["cover.png"] = "some text"
+sitecontent["moreDescription.html"] = "some text"
+sitecontent["DrawingObserver.png"] = "some text"
+sitecontent["Editor.png"] = "some text"
+sitecontent["MissingAnchor.png"] = "some text"
+sitecontent["todo.html"] = "some text"
+
+
